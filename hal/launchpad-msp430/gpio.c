f78bd43d1a52faeb3eda3b38db7ab1607292dd48
Dhiraj Goel <dhiraj.goel@gmail.com>
2011-09-04 23:34:18 -0700

    Updated gpio.c and Makefile

--- hal/launchpad-msp430/gpio.c
+++ hal/launchpad-msp430/gpio.c
@@ -1,9 +1,6 @@
-
-
 #include <gpio.h>
 #include <io.h>
 #include "launchpad-msp430.h"
-
 
 void gpio_init(unsigned int port, unsigned int num, GPIOConfig config)
 {
@@ -12,22 +9,41 @@ void gpio_init(unsigned int port, unsigned int num, GPIOConfig config)
             P1DIR |= (1<<num);
         }
     }
+    if (config & GPIO_INPUT) {
+        if (port == 1) {
+            P1DIR &= ~(1<<num);
+        }
+    }
+    if ((config & GPIO_PULL_UP) || (config & GPIO_PULL_DOWN)) {
+        if (port == 1) {
+            P1REN |= (1<<num);
+        }
+    }
 }
 
 void gpio_set_high(unsigned int port, unsigned int num)
 {
+    if (port == 1) {
+        P1OUT |= (1<<num);
+    }
 }
 
 void gpio_set_low(unsigned int port, unsigned int num)
 {
+    if (port == 1) {
+        P1OUT &= ~(1<<num);
+    }
 }
 
 unsigned char gpio_is_low(unsigned int port, unsigned int num)
 {
+    if (port == 1) {
+        return ((P1IN & (1<<num)) == 0);
+    }
     return 0;
 }
 
 unsigned char gpio_is_high(unsigned int port, unsigned int num)
 {
-    return 0;
+    return !gpio_is_low(port, num);
 }
