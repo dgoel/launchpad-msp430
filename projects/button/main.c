816bf5cf025f0da7a9b355d280a3b97f898348b6
Dhiraj Goel <dhiraj.goel@gmail.com>
2011-09-05 18:43:16 -0700

    Use the new button handler interface

--- projects/button/main.c
+++ projects/button/main.c
@@ -9,23 +9,19 @@ static void button_update(void)
     led_toggle(LED_RED);
 }
 
-
 int main(void)
 {
     system_init();
     led_init();
     button_init();
     interrupts_configure();
-
-    interrupts_disable();
-    interrupt_set_handler(IRQ_GPIO_PORT1_3, button_update);
-    irq_configure(IRQ_GPIO_PORT1_3, 1);
     interrupts_enable();
+
+    button_set_handler(button_update, BUTTON_PRESSED);
 
     led_on(LED_RED);
     led_on(LED_GREEN);
 
-    while (1) {
-    }
+    while (1) {;}
     return 0;
 }
