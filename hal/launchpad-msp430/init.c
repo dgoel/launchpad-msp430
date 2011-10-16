19171b889c7f2507e13590e72d83c5d979f85a27
Dhiraj Goel <dhiraj.goel@gmail.com>
2011-10-15 23:36:37 -0700

    Set up the clocks

--- hal/launchpad-msp430/init.c
+++ hal/launchpad-msp430/init.c
@@ -6,5 +6,22 @@
 void system_init(void)
 {
     WDTCTL = WDTPW + WDTHOLD;	// Stop WDT
-    BCSCTL3 |= LFXT1S_2;	//Set ACLK to use internal VLO (12 kHz clock)
+
+	//BCSCTL1 = CALBC1_1MHZ;			// Set range
+	// DCOCTL = CALDCO_1MHZ;			// SMCLK = DCO = 1MHz  
+
+    DCOCTL |= (DCO0 | DCO1 | DCO2); // highest frequency
+    BCSCTL1 |= (RSEL0 | RSEL1 | RSEL2); // config DCOCLK as 1MHz
+
+    // set ACLK divider to nothing
+    BCSCTL1 &= ~(DIVA0 | DIVA1);
+
+    // select DCOCLK as MCLK with no divider
+    BCSCTL2 &= ~(SELM0 | SELM1 | DIVM_0);
+
+    // select DCOCLK as SMCLK source with no divider
+    BCSCTL2 &= ~(SELS | DIVS_3);
+
+    // set ACLK to use internal VLO (12 kHz clock)
+    BCSCTL3 |= LFXT1S_2;
 }
